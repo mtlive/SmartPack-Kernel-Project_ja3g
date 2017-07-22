b8b9c995b64614249e984b0ce12ed0ca6556ed18
sunilpaulmathew <sunil.kde@gmail.com>
2017-07-22 22:29:14 +0900

    cleaning repo...
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- build_kernel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
-#rm log.txt
-#export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.9/bin/aarch64-linux-android-
-export ARCH=arm
-
-make make ja3g_00_defconfig
-make -j64
--- build_kernel_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export ARCH=arm
-export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.8/bin/arm-eabi-
-
-make -j8 ARCH=arm lineageos_i9500_defconfig
-make -j8 ARCH=arm
