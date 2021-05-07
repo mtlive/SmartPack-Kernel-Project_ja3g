fb1de85d4cc8fbbd7a6c2b7229af8d4dcd991339
mtlive <m.t_79@hotmail.com>
2021-05-07 22:58:14 +0430

    Update workqueue.c

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -252,7 +252,7 @@ struct workqueue_struct {
 };
 
 /* see the comment above the definition of WQ_POWER_EFFICIENT */
-//static bool wq_power_efficient = IS_ENABLED(CONFIG_WQ_POWER_EFFICIENT_DEFAULT);
+static bool wq_power_efficient; //= IS_ENABLED(CONFIG_WQ_POWER_EFFICIENT_DEFAULT);
 module_param_named(power_efficient, wq_power_efficient, bool, 0644);
 
 struct workqueue_struct *system_wq __read_mostly;
